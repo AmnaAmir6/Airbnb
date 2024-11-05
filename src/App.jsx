fd2a4826fd7776855d6453f03e70657875d79405
AmnaAmir6 <amnaamir605@gmail.com>
2024-11-06 00:22:57 +0500

    React Router Integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import {BrowserRouter, Routes,Route}from 'react-router-dom'
 import HomePage from "./pages/HomePage"
+import ListingDetailsPage from './pages/ListingDetailsPage'
+import BookingPage from './pages/BookingPage'
+
 function App () {
 
   return (
     <>
-
-   <HomePage/>
-
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/listings/:id' element={<ListingDetailsPage/>}/>
+        <Route path='/book/:id' element={<BookingPage/>}/>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/ListingDetailsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListingDetailsPage = () => {
+  return (
+    <div>ListingDetailsPage</div>
+  )
+}
+
+export default ListingDetailsPage
