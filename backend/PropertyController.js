76291c8ea008f9cfce46b270d74aaf3f775d0e44
AmnaAmir6 <amnaamir605@gmail.com>
2024-11-05 13:52:37 +0500

    setting api

---
+++ backend/PropertyController.js
@@ -0,0 +1,14 @@
+export const GetAllListings=async(req,res,next)=>{
+    console.log("GetAllListings function");
+    next();
+}
+
+export const GetListingsDetails=async(req,res,next)=>{
+    console.log("GetListingsDetails function");
+    next();
+}
+
+export const BookProperty=async(req,res,next)=>{
+    console.log("BookProperty function");
+    next();
+}
